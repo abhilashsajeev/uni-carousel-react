e40987b0cabe8d06e5557b8eb1359e6c5342e6ff
Abhilash <abhilash@qburst.com>
2015-08-20 10:13:17 +0530

    Enabled mouse dragging in example

--- index.js
+++ index.js
@@ -2,16 +2,23 @@ var React = require('react');
 var Carousel = require('uni-carousel-react');
 var CarouselSlider = React.createClass({
   render:function(){
+    
     return (
     	<Carousel slidesToShow={3} cellAlign="left"  edgeEasing="linear"
-			dragging = {false} speed={500}  framePadding="0px" cellSpacing={0}
+			dragging = {true} speed={500}  framePadding="0px" cellSpacing={0}
 			decorators={[]}>
-			<img src="http://placehold.it/1000x400&text=slide1"/>
-	        <img src="http://placehold.it/1000x400&text=slide2"/>
-	        <img src="http://placehold.it/1000x400&text=slide3"/>
-	        <img src="http://placehold.it/1000x400&text=slide4"/>
-	        <img src="http://placehold.it/1000x400&text=slide5"/>
-	        <img src="http://placehold.it/1000x400&text=slide6"/>
+			<img src="http://lorempixel.com/500/500/nature/1/"/>
+	        <img src="http://lorempixel.com/500/500/nature/2/"/>
+	        <img src="http://lorempixel.com/500/500/nature/3/"/>
+	        <img src="http://lorempixel.com/500/500/nature/4/"/>
+	        <img src="http://lorempixel.com/500/500/nature/5/"/>
+	        <img src="http://lorempixel.com/500/500/nature/6/"/>
+	        <img src="http://lorempixel.com/500/500/nature/7/"/>
+	        <img src="http://lorempixel.com/500/500/nature/8/"/>
+	        <img src="http://lorempixel.com/500/500/nature/9/"/>
+	        <img src="http://lorempixel.com/500/500/sports/1/"/>
+	        <img src="http://lorempixel.com/500/500/sports/2/"/>
+	        <img src="http://lorempixel.com/500/500/sports/3/"/>
     	</Carousel>
     	);
   }
--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 var React = require('react');
 var Carousel = require('uni-carousel-react');
 var CarouselSlider = React.createClass({displayName: "CarouselSlider",
+  mixins:[Carousel.ControllerMixin],
   render:function(){
     return (
-    	React.createElement(Carousel, {slidesToShow: 3, cellAlign: "left", edgeEasing: "linear", 
-			dragging: false, speed: 500, framePadding: "0px", cellSpacing: 20, 
-			decorators: []}, 
-			React.createElement("img", {src: "http://placehold.it/1000x400&text=slide1"}), 
-	        React.createElement("img", {src: "http://placehold.it/1000x400&text=slide2"}), 
-	        React.createElement("img", {src: "http://placehold.it/1000x400&text=slide3"}), 
-	        React.createElement("img", {src: "http://placehold.it/1000x400&text=slide4"}), 
-	        React.createElement("img", {src: "http://placehold.it/1000x400&text=slide5"}), 
-	        React.createElement("img", {src: "http://placehold.it/1000x400&text=slide6"})
+    	React.createElement(Carousel, {slidesToShow: 1, cellAlign: "left", edgeEasing: "linear", 
+			dragging: true, speed: 500, framePadding: "0px", cellSpacing: 20, decorators:[]
+			}, 
+			React.createElement("img", {src: "http://lorempixel.com/500/500/nature/1/"}), 
+	        React.createElement("img", {src: "http://lorempixel.com/500/500/nature/2/"}), 
+	        React.createElement("img", {src: "http://lorempixel.com/500/500/nature/3/"}), 
+	        React.createElement("img", {src: "http://lorempixel.com/500/500/nature/4/"}), 
+	        React.createElement("img", {src: "http://lorempixel.com/500/500/nature/5/"}), 
+	        React.createElement("img", {src: "http://lorempixel.com/500/500/nature/6/"}),
+	        React.createElement("img", {src: "http://lorempixel.com/500/500/nature/7/"}),
+	        React.createElement("img", {src: "http://lorempixel.com/500/500/nature/8/"}),
+	        React.createElement("img", {src: "http://lorempixel.com/500/500/nature/9/"}),
+	        React.createElement("img", {src: "http://lorempixel.com/500/500/sports/1/"}),
+	        React.createElement("img", {src: "http://lorempixel.com/500/500/sports/2/"}),
+	        React.createElement("img", {src: "http://lorempixel.com/500/500/sports/3/"})
     	)
     	);
   }
