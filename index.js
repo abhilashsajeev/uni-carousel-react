26a2102524960445a20f208a66ab40ff81cf457e
Abhilash <abhilashsajeev@users.noreply.github.com>
2016-03-18 10:43:56 +0530

    Update for 0.14

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
-var React = require('react');
-var Carousel = require('uni-carousel-react');
+'use strict';
+
+import Carousel from '../src/carousel';
+import React from 'react';
+import ReactDom from 'react-dom';
+
+window.React = React;
+
+
+
+const content = document.getElementById('main');
+
+
 var CarouselSlider = React.createClass({
   render:function(){
-    
     return (
-    	<Carousel slidesToShow={3} cellAlign="left"  edgeEasing="linear"
-			dragging = {true} speed={500}  framePadding="0px" cellSpacing={0}
-			decorators={[]}>
-			<img src="http://lorempixel.com/500/500/nature/1/"/>
-	        <img src="http://lorempixel.com/500/500/nature/2/"/>
-	        <img src="http://lorempixel.com/500/500/nature/3/"/>
-	        <img src="http://lorempixel.com/500/500/nature/4/"/>
-	        <img src="http://lorempixel.com/500/500/nature/5/"/>
-	        <img src="http://lorempixel.com/500/500/nature/6/"/>
-	        <img src="http://lorempixel.com/500/500/nature/7/"/>
-	        <img src="http://lorempixel.com/500/500/nature/8/"/>
-	        <img src="http://lorempixel.com/500/500/city/9/"/>
-	        <img src="http://lorempixel.com/500/500/sports/1/"/>
-	        <img src="http://lorempixel.com/500/500/sports/2/"/>
-	        <img src="http://lorempixel.com/500/500/sports/3/"/>
-    	</Carousel>
-    	);
+      <Carousel slidesToShow={3} cellAlign="left"  edgeEasing="linear"
+      dragging = {true} speed={500}  framePadding="0px" cellSpacing={0}
+      decorators={[]}>
+          <img src="http://lorempixel.com/500/500/nature/1/"/>
+          <img src="http://lorempixel.com/500/500/nature/2/"/>
+          <img src="http://lorempixel.com/500/500/nature/3/"/>
+          <img src="http://lorempixel.com/500/500/nature/4/"/>
+          <img src="http://lorempixel.com/500/500/nature/5/"/>
+          <img src="http://lorempixel.com/500/500/nature/6/"/>
+          <img src="http://lorempixel.com/500/500/nature/7/"/>
+          <img src="http://lorempixel.com/500/500/nature/8/"/>
+          <img src="http://lorempixel.com/500/500/city/9/"/>
+          <img src="http://lorempixel.com/500/500/sports/1/"/>
+          <img src="http://lorempixel.com/500/500/sports/2/"/>
+          <img src="http://lorempixel.com/500/500/sports/3/"/>
+      </Carousel>
+      );
   }
 });
-React.render(<CarouselSlider/>,document.getElementById('main'));
+
+ReactDom.render(<CarouselSlider/>, content)
